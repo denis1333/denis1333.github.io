b1743caa45253467cb4623d919faa20edd4b6785
denis1333 <pankov_den@mail.ru>
2017-09-24 17:57:06 +0300

    fix

--- port2/assets/js/app.js
+++ port2/assets/js/app.js
@@ -1,10 +1,9 @@
 $( document ).ready(function() {
-	$('input[type=range]').attr('max', 0);
-	$('input[type=range]').attr('min', -$('.elements').width()/2 - 100);
-	$('input[type=range]').attr('value', -$('.elements').width()/4);
-	$('.elements').css('left', -$('.elements').width()/4);
+	$('input[type=range]').attr('max', $('.elements').width()/2 + 100);
+	$('input[type=range]').attr('min', 0);
+	$('input[type=range]').attr('value', 0);
 
 	$('input[type=range]').on("change mousemove", function(){
-		$('.elements').css('left', $(this).val()+'px')
+		$('.elements').css('left', -$(this).val()+'px')
 	});
 });
